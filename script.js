d559040850912476280bf96cf1761d502157df2a
Tenma <redoc9527@gmail.com>
2025-07-10 10:18:52 +0800

    完成 在页面显示对局,以及对局完成后重开逻辑

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const choice = document.querySelector("#choice");
 choice.addEventListener("click", (event) => {
   playRound(event.target.id);
 });
+const showList = document.querySelector("#show");
 function getComputerChoice() {
   let num = Math.random() * 3;
   if (num < 1) {
@@ -29,33 +30,43 @@ function getHumanChoice(id) {
 function playRound(id) {
   let human = getHumanChoice(id);
   let computer = getComputerChoice();
+  show(human, computer);
+}
+
+function show(human, computer) {
+  //对局结束后不再处理
+  if (humanScore == 5 || computerScore == 5) return;
+  let mes = document.createElement("li");
   if (human === computer) {
-    console.log(`平局`);
+    mes.textContent = `平局`;
   } else if (
     (human === "石头" && computer === "剪刀") ||
     (human === "剪刀" && computer === "布") ||
     (human === "布" && computer === "石头")
   ) {
-    console.log(`你赢了！${human}战胜了${computer}`);
+    mes.textContent = `你赢了！${human}战胜了${computer}`;
     humanScore++;
   } else {
-    console.log(`你输了！${human}输给了${computer}`);
+    mes.textContent = `你输了！${human}输给了${computer}`;
     computerScore++;
   }
-}
-
-function playGame() {
-  playRound();
-  playRound();
-  playRound();
-  playRound();
-  playRound();
-  console.log(`computer:${computerScore}分，你:${humanScore}分`);
-  if (computerScore > humanScore) {
-    console.log("你输了");
-  } else if (computerScore < humanScore) {
-    console.log("你赢了");
-  } else {
-    console.log("平局");
+  showList.appendChild(mes);
+  //对局结束后添加 p 和 button
+  if (humanScore == 5 || computerScore == 5) {
+    let hit = document.createElement("p");
+    hit.textContent = `你总分：${humanScore}，电脑总分：${computerScore}，你${
+      humanScore == 5 ? "赢" : "输"
+    }了`;
+    document.body.appendChild(hit);
+    let restart = document.createElement("button");
+    restart.textContent = "重开";
+    document.body.appendChild(restart);
+    restart.addEventListener("click", () => {
+      humanScore = 0;
+      computerScore = 0;
+      showList.replaceChildren();
+      document.body.removeChild(hit);
+      document.body.removeChild(restart);
+    });
   }
 }
